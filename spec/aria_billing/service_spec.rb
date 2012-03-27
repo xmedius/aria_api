c4f87152dc38a9875a51ec8703bcee8f4c8b4db9
jpemberthy <jpemberthy@gmail.com>
2012-03-26 22:14:47 -0500

    uses the version 5.14 of the AriaApi. 3 Failures.

--- spec/aria_billing/service_spec.rb
+++ spec/aria_billing/service_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe AriaBilling::Service do
 
   describe "self.actions", :vcr do
-    it "returns an array with all the available soap actions" do
+    it "returns an array with all the available soap actions", :vcr do
       actions = AriaBilling::Service.actions
       calls = [:get_parent_for_item_class, :validate_session, :get_statement_content, :get_child_accts]
       actions.should include(calls.sample)
