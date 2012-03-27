2bbd3cc25fe834a09c4fb963ebf5c9709592a294
jpemberthy <jpemberthy@gmail.com>
2012-03-27 15:21:57 -0500

    Use aria 5.14.

--- lib/aria_billing/service.rb
+++ lib/aria_billing/service.rb
@@ -1,7 +1,7 @@
 module AriaBilling
   class Service
     def self.actions
-      url = "https://secure.future.stage.ariasystems.net/api/Advanced/wsdl/5.7/complete-doc_literal_wrapped.wsdl"
+      url = "https://secure.future.stage.ariasystems.net/api/Advanced/wsdl/5.14/complete-doc_literal_wrapped.wsdl"
       client = Savon::Client.new url
       client.wsdl.soap_actions
     end
