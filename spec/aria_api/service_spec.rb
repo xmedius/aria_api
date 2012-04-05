4a44f6829f34e1cc11b79af9cc7711be0e6b9136
jpemberthy <jpemberthy@gmail.com>
2012-04-05 13:15:18 -0500

    1.8.7 support for service_spec.

--- spec/aria_api/service_spec.rb
+++ spec/aria_api/service_spec.rb
@@ -6,7 +6,11 @@ describe AriaApi::Service do
     it "returns an array with all the available soap actions", :vcr do
       actions = AriaApi::Service.actions
       calls = [:get_parent_for_item_class, :validate_session, :get_statement_content, :get_child_accts]
-      actions.should include(calls.sample)
+      actions.should include(calls.send(random_sample))
+    end
+
+    def random_sample
+      RUBY_VERSION >= "1.9" ? :sample : :choice
     end
   end
 end
