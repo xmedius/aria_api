1a54c9c5479d3eff8f5af96045d89c3e134a91b9
Pete Broderick <simplybinary@gmail.com>
2013-05-21 15:43:40 -0400

    Updating AriaApi::Service to use the wsdl_path configuration to optionally load a local wsdl file in order to avoid making a web request

--- lib/aria_api/service.rb
+++ lib/aria_api/service.rb
@@ -1,10 +1,29 @@
 module AriaApi
   class Service
-    def self.actions
-      api_version = AriaApi::Configuration.api_version
-      url = "https://secure.future.stage.ariasystems.net/api/Advanced/wsdl/#{api_version}/complete-doc_literal_wrapped.wsdl"
-      client = Savon.client(wsdl: url)
-      @actions ||= client.operations
+    class << self
+      def actions
+        @actions ||= client.operations
+      end
+
+      private
+
+      def client
+        if AriaApi::Configuration.wsdl_path
+          local_client
+        else
+          remote_client
+        end
+      end
+
+      def local_client
+        Savon.client(wsdl: File.open(AriaApi::Configuration.wsdl_path))
+      end
+
+      def remote_client
+        api_version = AriaApi::Configuration.api_version
+        url = "https://secure.future.stage.ariasystems.net/api/Advanced/wsdl/#{api_version}/complete-doc_literal_wrapped.wsdl"
+        Savon.client(wsdl: url)
+      end
     end
   end
 end
